fdc9ad14229a8212eb2e783efba46c4550104769
IamAldrei <kimaldreic@gmail.com>
2024-07-04 16:05:03 +0800

    modified the index.html style.css and script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.console.log();
+document.addEventListener('DOMContentLoaded', function() {
+
+    const navLinks = document.querySelectorAll('.navbar a');
+
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function(event) {
+            event.preventDefault();
+
+
+            console.log('Clicked:', navLink.id);
+
+        });
+    });
+});
