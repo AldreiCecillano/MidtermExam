b18471f4fe391f49920494fce7228e3e1ea6e932
IamAldrei <kimaldreic@gmail.com>
2024-07-04 16:25:46 +0800

    edit all

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    const navLinks = document.querySelectorAll('.navbar a');
-
-
-    navLinks.forEach(function(navLink) {
-        navLink.addEventListener('click', function(event) {
-            event.preventDefault();
-
-
-            console.log('Clicked:', navLink.id);
-
-        });
-    });
-});
+console.console.log();
